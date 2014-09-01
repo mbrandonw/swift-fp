28882ada9c755433d158310284b728da7059566f
mbrandonw <mbw234@gmail.com>
2014-09-01 18:40:30 -0400

    Make some tuples equatable.

--- swift-fp/Tuples.swift
+++ swift-fp/Tuples.swift
@@ -123,6 +123,35 @@ func _1 <A, B> (b: B, x: (A, B)) -> (A, B) {
 }
 
 /**
+ Equatable
+ */
+func == <A: Equatable, B: Equatable> (lhs:(A, B), rhs:(A, B)) -> Bool {
+  return lhs.0 == rhs.0
+    && lhs.1 == rhs.1
+}
+
+func == <A: Equatable, B: Equatable, C: Equatable> (lhs:(A, B, C), rhs:(A, B, C)) -> Bool {
+  return lhs.0 == rhs.0
+    && lhs.1 == rhs.1
+    && lhs.2 == rhs.2
+}
+
+func == <A: Equatable, B: Equatable, C: Equatable, D: Equatable> (lhs:(A, B, C, D), rhs:(A, B, C, D)) -> Bool {
+  return lhs.0 == rhs.0
+    && lhs.1 == rhs.1
+    && lhs.2 == rhs.2
+    && lhs.3 == rhs.3
+}
+
+func == <A: Equatable, B: Equatable, C: Equatable, D: Equatable, E: Equatable> (lhs:(A, B, C, D, E), rhs:(A, B, C, D, E)) -> Bool {
+  return lhs.0 == rhs.0
+    && lhs.1 == rhs.1
+    && lhs.2 == rhs.2
+    && lhs.3 == rhs.3
+    && lhs.4 == rhs.4
+}
+
+/**
  Flip
  
  Precompose with a function (B, A) -> C to turn it into
